2814a18d77f4621edb473a15ff191e55a7671c41
SamyMechiche <mechiche.samysm@gmail.com>
2025-01-20 16:45:34 +0100

    logged into the DB

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ECF-2
 Gestion d'une to-do list partagée
+
+lien figma: https://www.figma.com/design/Ym08e9Uqb35GizNqK067gq/ECF-2-TO-DO-list?node-id=0-1&p=f&t=lqnj3lHGfq5P5Q5V-0
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+include_once('./connexion.php');
+var_dump($bdd);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TeamTasker</title>
+    <meta name="description" content="Homepage de TeamTasker">
+    <link rel="stylesheet" href="./asset./css/style.css">
+</head>
+<body>
+    
+<script src="./asset./js/main.js"></script>
+</body>
+</html>
